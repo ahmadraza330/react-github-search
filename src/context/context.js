bb2e5e87cdcef1aad9e51f605b61ea954f8ab3c4
Ahmad Raza <74761556+ahmadraza330@users.noreply.github.com>
2021-03-17 13:05:08 +0500

    Craeted project with react and fetch github api

---
+++ src/context/context.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import mockUser from './mockData.js/mockUser';
+import mockRepos from './mockData.js/mockRepos';
+import mockFollowers from './mockData.js/mockFollowers';
+import axios from 'axios';
+
+const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({children}) => {
+    const [githubUser, setGithubUser] = useState(mockUser);
+    const [repos, setRepos] = useState(mockRepos);
+    const [followers, setFlollower] = useState(mockFollowers);
+    const [requests, setRequest] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState({show:false, msg: ''});
+
+    const searchGithubUser = async(user) => {
+        toggleError();
+        setIsLoading(true);
+        const response = await axios(`${rootUrl}/users/${user}`).catch(err => console.log(err));
+        if(response) {
+            setGithubUser(response.data);
+            const {login, followers_url} = response.data;
+            //repo
+           await Promise.allSettled(
+               [
+                axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+                axios(`${followers_url}?per_page=100`),
+
+               ]).then((results) => {
+               const [repos, followers] = results;
+               const status = 'fulfilled';
+               if(repos.status === status) {
+                   setRepos(repos.value.data);
+               }
+               if(followers.status === status) {
+                   setFlollower(followers.value.data);
+               }
+           }).catch(err => console.log(err)); 
+
+        } else {
+            toggleError(true, 'there is no user with that username');
+        }
+        checkRequest();
+        setIsLoading(false);
+    }
+
+    const checkRequest = () => {
+        axios(`${rootUrl}/rate_limit`).then(({data}) => {
+            let {rate: {remaining}} = data;
+            setRequest(remaining);
+            if(remaining === 0) {
+                toggleError(true, 'sorry, you exceeded your hourly rate limit');
+            }
+        }).catch((err) => console.log(err));
+    }
+
+    function toggleError(show = false, msg = '') {
+        setError({show, msg})
+    }
+
+    useEffect(checkRequest, []);
+
+    return (<GithubContext.Provider value={{githubUser, repos, followers, requests, isLoading, error, searchGithubUser}}>
+        {children}
+    </GithubContext.Provider>
+    )
+}
+
+export {GithubProvider, GithubContext};
